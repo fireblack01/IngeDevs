d300660f41a821faa9d3bc429e1898769e4b46bc
Michael H. Tapia A <ziborsoft@gmail.com>
2021-09-29 20:24:58 -0500

    empecemos aqui

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,53 @@
-import logo from './logo.svg';
+import logo from './media/logo.png';
 import './styles/styles.css';
-//import 'bootstrap/dist/css/bootstrap.css';
+import borderCollie from './media/borderCollie.jpg';
+import rhodesian from './media/rhodesian.jpg';
 
 function App() {
   return (
-    <div className="App">
-        <header>Aqui va toda mi pagina</header>
-        <main>este es el main</main>
-        <footer>este es el footer</footer>
+    <div classnameName="App">
+      <header>
+        <ul classname="navbar">
+          <li>
+            <img src={logo} alt="imagen" classname="logo" />
+          </li>
+          <li>
+            <button classname="botonGenerico mainButton">Nuevo post</button>
+          </li>
+          <li>
+            <div classname="buscar">
+              <input placeholder="Buscar una raza" />
+              <i classname="fas fa-search botonGenerico iconoBusqueda"></i>
+            </div>
+          </li>
+            <li><button classname="botonGenerico secondaryButton">Login</button>
+          </li>
+          <li>
+            <button classname="botonGenerico mainButton">Registro</button>
+          </li>
+        </ul>
+      </header>
+      <main>
+        <section>
+          <h1>Razas de Perros</h1>
+          <ul classname='breedCardContainer'>
+            <li classname='breedCard'>
+              <div classname='contenedorImagen'>
+                <img src={borderCollie} alt='Border Collie' />
+              </div>
+              <span classname='breedTitle'>Border Collie </span>
+            </li>
+            <li classname="breedCard">
+              <div classname="contenedorImagen">
+                <img src={rhodesian} alt="Rhodesian" />
+              </div>
+              <span classname="breedTitle">Rhodesian</span>
+            </li>
+          </ul>
+        </section>
+        <section></section>
+      </main>
+      <footer></footer>
     </div>
   );
 }
