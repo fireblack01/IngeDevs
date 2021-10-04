162a5bfdb8bb5fd555bf82fa50f25c4214cb663a
Michael H. Tapia A <ziborsoft@gmail.com>
2021-10-04 15:14:13 -0500

    registro de productos

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,31 @@
-import logo from './media/logo.png';
 import './styles/styles.css';
-import borderCollie from './media/borderCollie.jpg';
-import rhodesian from './media/rhodesian.jpg';
+
 
 function App() {
   return (
     <div classnameName="App">
-      <header>
-        <ul classname="navbar">
-          <li>
-            <img src={logo} alt="imagen" classname="logo" />
-          </li>
-          <li>
-            <button classname="botonGenerico mainButton">Nuevo post</button>
-          </li>
-          <li>
-            <div classname="buscar">
-              <input placeholder="Buscar una raza" />
-              <i classname="fas fa-search botonGenerico iconoBusqueda"></i>
-            </div>
-          </li>
-            <li><button classname="botonGenerico secondaryButton">Login</button>
-          </li>
-          <li>
-            <button classname="botonGenerico mainButton">Registro</button>
-          </li>
-        </ul>
-      </header>
-      <main>
-        <section>
-          <h1>Razas de Perros</h1>
-          <ul classname='breedCardContainer'>
-            <li classname='breedCard'>
-              <div classname='contenedorImagen'>
-                <img src={borderCollie} alt='Border Collie' />
-              </div>
-              <span classname='breedTitle'>Border Collie </span>
-            </li>
-            <li classname="breedCard">
-              <div classname="contenedorImagen">
-                <img src={rhodesian} alt="Rhodesian" />
-              </div>
-              <span classname="breedTitle">Rhodesian</span>
-            </li>
-          </ul>
-        </section>
-        <section></section>
-      </main>
-      <footer></footer>
+<header>Aqui va el encabezado</header>
+        <main>
+          <h1>Administracíon de productos</h1>
+          <form>
+            <label for ="ID">Codigo </label>
+            <input type="text" placeholder="ID del producto" id="ID"></input><br></br><br></br>
+            <label for ="descripcion">Descripcion </label>
+            <input type="text" placeholder="Descripcion del producto" id="descripcion"></input><br></br><br></br>
+            <label for ="valor_unitario">Valor unitario </label>
+            <input type="text" placeholder="Valor unitario" id="valor_unitario"></input><br></br><br></br>
+            <label for="producto">Estado </label>
+            <select name="producto" id="producto">
+              <option value="volvo">Disponible</option>
+              <option value="saab">No Disponible</option>
+            </select><br></br><br></br>
+
+            <input type="submit" value="Registrar"></input>
+          </form>
+        
+        </main>
+        
+        <footer>este es el footer</footer>
     </div>
   );
 }
