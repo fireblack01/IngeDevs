402b36042765dbab39aae385c30ee0bb074fa21c
Michael H. Tapia A <ziborsoft@gmail.com>
2021-10-05 12:37:29 -0500

    actualizar pagina

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,27 @@
+import RhodesianInfoPage from 'pages/nuevoProducto';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './styles/styles.css';
 
 
 function App() {
   return (
     <div classnameName="App">
-<header>Aqui va el encabezado</header>
-        <main>
-          <h1>Administracíon de productos</h1>
-          <form>
-            <label for ="ID">Codigo </label>
-            <input type="text" placeholder="ID del producto" id="ID"></input><br></br><br></br>
-            <label for ="descripcion">Descripcion </label>
-            <input type="text" placeholder="Descripcion del producto" id="descripcion"></input><br></br><br></br>
-            <label for ="valor_unitario">Valor unitario </label>
-            <input type="text" placeholder="Valor unitario" id="valor_unitario"></input><br></br><br></br>
-            <label for="producto">Estado </label>
-            <select name="producto" id="producto">
-              <option value="volvo">Disponible</option>
-              <option value="saab">No Disponible</option>
-            </select><br></br><br></br>
+     
+      <header>Aqui va el encabezado</header>
 
-            <input type="submit" value="Registrar"></input>
-          </form>
+      <main>todo el contenido principal
+      <Router>
+          <Switch>
+            <Route path='/nuevoProducto'>
+              <nuevoProducto />
+            </Route>
+
+          </Switch>
+
+      </Router>
+      </main>
         
-        </main>
-        
-        <footer>este es el footer</footer>
+      <footer>este es el footer</footer>
     </div>
   );
 }
---
+++ src/pages/maestroProducto.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const maestroProducto = () => {
+  return <div>Listado de productos</div>;
+};
+
+export default maestroProducto;
