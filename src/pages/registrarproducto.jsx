a5f6c3c14b248980f7e37106aa443d8b9f27b795
DESKTOP-74NHKNA\ander <amlandazuric@gmail.com>
2021-10-07 15:26:54 -0500

    arreglo de estilos

--- src/pages/registrarproducto.jsx
+++ src/pages/registrarproducto.jsx
@@ -2,55 +2,23 @@ import { Link } from 'react-router-dom';
 const RegistrarProducto = () => {
     return(    
         <div className="content">
-            <div className="container mw-60 pd-tb-30">    
+            <div className="container mw-60 pd-tb-30 flex-form">    
                 <h2>Administración de productos</h2>
-                <div className="PosButtonL"><Link to="maestroProducto"><button className="button">Listar productos</button></Link></div>
                 <form id="transactionForm">
-                    <table border="1px">
-                        <tr>
-                            <td><label for ="ID">Código </label></td>
-                            <td><input type="text" placeholder="ID del producto" id="ID" name="ID"required/></td>
-                        </tr>
-                        <tr>  
-                            <td><label for ="descripcion">Descripción </label></td>
-                            <td><input type="text" placeholder="Descripcion del producto" id="descripcion" name="descripcion"required/></td>
-                        </tr>
-                        <tr>  
-                            <td><label for ="valor_unitario">Valor Unitario </label></td>
-                            <td><input type="number" placeholder="Valor Unitario" id="valor_unitario" name="valor_unitario"required/></td>
-                        </tr>
-                        <tr> 
-                            <td><label for="estado">Estado </label></td>
-                            <td>
-                                <select name="estado" id="estado">
-                                <option value="Disponible">Disponible</option>
-                                <option value="No disponible">No Disponible</option>
-                                </select>
-                            </td>
-                        </tr> 
-                    </table>
-
-                    
-
+                    <label for ="ID">Código </label>
+                    <input type="text" placeholder="ID del producto" id="ID" name="ID"required/>
+                    <label for ="descripcion">Descripción </label>
+                    <input type="text" placeholder="Descripcion del producto" id="descripcion" name="descripcion"required/>
+                    <label for ="valor_unitario">Valor Unitario </label>
+                    <input type="number" placeholder="Valor Unitario" id="valor_unitario" name="valor_unitario"required/>
+                    <label for="estado">Estado </label>
+                    <select name="estado" id="estado">
+                        <option value="Disponible">Disponible</option>
+                        <option value="No disponible">No Disponible</option>
+                    </select>
+                    <Link type="submit" className="button">Registrar</Link>
                 </form>
-                <button className="button" id="botonDeEnvio">Registrar</button>
-                <br></br><br></br>
-
-                    <table border="1px" id="transactionTable">
-                        <tr>
-                            <th>Código</th>
-                            <th>Descripción</th>
-                            <th>Valor Unitario</th>
-                            <th>Estado</th>
-                        </tr>
-                        <tr>
-                            <td align="center">001</td>
-                            <td align="center">Tenis</td>
-                            <td align="center">12000</td>
-                            <td align="center">Disponible</td>
-                        </tr>
-                    </table>
-
+                <Link to="maestroProducto" className="button listar">Listar productos</Link>
             </div>        
         </div>   
     );
