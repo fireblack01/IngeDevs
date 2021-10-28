16c05b59f64f843e99a7fa48097d103c35c3cdf0
DESKTOP-74NHKNA\ander <amlandazuric@gmail.com>
2021-10-28 05:40:11 -0500

    fix conflicts

--- src/pages/content.jsx
+++
@@ -1,9 +0,0 @@
-import ContenidoInicio from 'components/ContenidoInicio';
-import React from 'react';
-const Content = () => {
-  return (
-    <ContenidoInicio />
-  );
-}
-
-export default Content;
--- src/pages/registrarventa.jsx
+++
@@ -1,6 +0,0 @@
-const RegistrarVenta = () => {
-    return(
-        <div className="rojo">Aca voy a registrar una venta</div>
-    )
-}
-export default RegistrarVenta;
